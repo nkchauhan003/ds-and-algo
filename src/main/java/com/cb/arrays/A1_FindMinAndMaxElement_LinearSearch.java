4f3ec32c2653f022f5fc7d8dfda0c46df12ff442
nkchauhan003 <nkchauhan003@gmail.com>
2022-09-17 11:50:08 +0530

    added new problems

--- src/main/java/com/cb/arrays/A1_FindMinAndMaxElement_LinearSearch.java
+++ src/main/java/com/cb/arrays/A1_FindMinAndMaxElement_LinearSearch.java
@@ -5,39 +5,40 @@ import java.util.stream.Collectors;
 
 /*
  * Linear Search
+ * Time Complexity: O(n)
  * */
 public class A1_FindMinAndMaxElement_LinearSearch {
 
-    public static int[] findMinMax(int[] input) {
+    public static long[] findMinMax(long[] input) {
 
         // return [-1, -1] for invalid input array
         if (input == null || input.length < 1)
-            return new int[]{-1, -1};
+            return new long[]{-1, -1};
 
-        // initialize with 1st element or array
-        int min = input[0];
-        int max = input[0];
+        // initialize with extreme values
+        long min = Long.MAX_VALUE;
+        long max = 0;
 
         // start iteration from 2nd element
-        for (int i = 1; i < input.length; i++) {
+        for (int i = 0; i < input.length; i++) {
             if (input[i] > max)
                 max = input[i];
             if (input[i] < min)
                 min = input[i];
         }
-        return new int[]{min, max};
+        return new long[]{min, max};
     }
 
     // Testing
     public static void main(String[] args) {
-        printArray(findMinMax(new int[]{}));
-        printArray(findMinMax(new int[]{1, 2, 3, 4, 5}));
-        printArray(findMinMax(new int[]{1}));
-        printArray(findMinMax(new int[]{52, 94, 3, 18, 5}));
+        printArray(findMinMax(new long[]{}));
+        printArray(findMinMax(new long[]{1, 2, 3, 4, 5}));
+        printArray(findMinMax(new long[]{1}));
+        printArray(findMinMax(new long[]{52, 94, 3, 18, 5}));
     }
 
     // Just to print output array
-    private static void printArray(int[] arr) {
-        System.out.println(Arrays.stream(arr).mapToObj(Integer::toString).collect(Collectors.joining(",")));
+    private static void printArray(long[] arr) {
+        System.out.println(Arrays.stream(arr).mapToObj(Long::toString).collect(Collectors.joining(",")));
     }
 }
---
+++ src/main/java/com/cb/arrays/A8_KadanesAlgorithm_Print_Subarray.java
@@ -0,0 +1,46 @@
+package com.cb.arrays;
+
+/*
+ * Kadane's Algorithm
+ * Time: O(n)
+ * Space: O(1)
+ * */
+public class A8_KadanesAlgorithm_Print_Subarray {
+    public static void contiguousSubArray(int arr[], int n) {
+
+        // Start of contiguous-SubArray
+        int start = 0, newStart = 0;
+
+        // End of contiguous-SubArray
+        int end = 0;
+        int maxSoFar = 0;
+        int maxEndingHere = 0;
+
+        for (int i = 0; i < n; i++) {
+            maxEndingHere += arr[i];
+            if (maxEndingHere < 0) {
+                maxEndingHere = 0;
+                newStart = i + 1;
+            }
+            if (maxEndingHere > maxSoFar) {
+                maxSoFar = maxEndingHere;
+                start = newStart;
+                end = i;
+            }
+        }
+        System.out.println("Sub-array: { " + print(arr, start, end) + "}, Sum: " + maxSoFar);
+    }
+
+    // Just to print sub-array
+    private static String print(int[] arr, int start, int end) {
+        String s = "";
+        for (int i = start; i <= end; i++)
+            s += arr[i] + " ";
+        return s;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
+        contiguousSubArray(arr, arr.length); // Sub-array: { 4 -1 -2 1 5 }, Sum: 7
+    }
+}
