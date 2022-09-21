52528cb7240d6c6157d70ab560decb33ddb7b261
nkchauhan003 <nkchauhan003@gmail.com>
2022-09-21 20:00:31 +0530

    added new problems

--- src/main/java/com/cb/linkedlist/LL5_RemoveDuplicatesFromUnsortedLinkedList.java
+++ src/main/java/com/cb/linkedlist/LL5_RemoveDuplicatesFromUnsortedLinkedList.java
@@ -25,16 +25,18 @@ public class LL5_RemoveDuplicatesFromUnsortedLinkedList {
 
     public static void main(String[] args) {
 
-        // 1->2->3->4->5
-        Node head = new Node(5);
+        // 1->2->2->3->2->null
+        Node head = new Node(1);
         head.next = new Node(2);
         head.next.next = new Node(2);
-        head.next.next.next = new Node(4);
+        head.next.next.next = new Node(3);
+        head.next.next.next.next = new Node(2);
+
 
         // traverse
-        traverse(head);
+        traverse(head); // 1 2 2 3 2
         removeDuplicate(head);
-        traverse(head);
+        traverse(head); // 1 2 3
     }
 
     public static void traverse(Node<Integer> head) {
