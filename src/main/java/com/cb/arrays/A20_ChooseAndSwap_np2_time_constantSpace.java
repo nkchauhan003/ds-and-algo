1d1ae4b75bd0aac6cb714bb2205f8688bbbb2337
nkchauhan003 <nkchauhan003@gmail.com>
2022-09-25 12:11:29 +0530

    added new problems

---
+++ src/main/java/com/cb/arrays/A20_ChooseAndSwap_np2_time_constantSpace.java
@@ -0,0 +1,59 @@
+package com.cb.arrays;
+
+/*
+ * Choose and Swap
+ * Time: O(n^2)
+ * Space: O(1)
+ * */
+public class A20_ChooseAndSwap_np2_time_constantSpace {
+    public static void rearrange(char[] s, int n) {
+
+        Character c1 = null, c2 = null;
+        for (int i = 0; i < n - 1; i++) {
+            char c = s[i];
+            int j = i + 1;
+            while (j < n) {
+                if (s[j] < c && (c2 == null || s[j] < c2)) {
+                    int k = i - 1;
+                    boolean isGood = true;
+                    while (k >= 0) {
+                        if (s[k] == s[j]) {
+                            isGood = false;
+                            break;
+                        }
+                        k--;
+                    }
+
+                    if (isGood) {
+                        c1 = s[i];
+                        c2 = s[j];
+                    }
+                }
+                j++;
+            }
+            if (c1 != null)
+                break;
+        }
+        //System.out.println(c1 + "," + c2);
+        if (c1 != null)
+            for (int i = 0; i < n; i++) {
+                if (s[i] == c1)
+                    s[i] = c2;
+                else if (s[i] == c2)
+                    s[i] = c1;
+
+            }
+    }
+
+
+    public static void main(String[] args) {
+        String s = "ccad";
+        int n = s.length();
+
+        char[] chars = s.toCharArray();
+        System.out.println(chars);
+        rearrange(chars, n);
+        System.out.println(chars);
+
+    }
+}
