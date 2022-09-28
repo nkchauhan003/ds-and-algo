b20052f02010521185a393ad6187d65cea1b3e8b
nkchauhan003 <nkchauhan003@gmail.com>
2022-09-28 16:47:21 +0530

    added new problems

---
+++ src/main/java/com/cb/matrix/M3_RotateMatrix_extraSpace.java
@@ -0,0 +1,41 @@
+package com.cb.matrix;
+
+/*
+ * Time: O(n^2)
+ * Space: O(n^2)
+ * */
+public class M3_RotateMatrix_extraSpace {
+
+    private static int[][] rotate(int[][] matrix, int m, int n) {
+
+        int[][] tmp = new int[n][m];
+
+        for (int j = 0; j < n; j++) {
+            for (int i = m - 1; i >= 0; i--) {
+                tmp[j][(m - 1) - i] = matrix[i][j];
+            }
+        }
+        return tmp;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] mtx = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
+        int m = mtx.length;
+        int n = m < 1 ? 0 : mtx[0].length;
+
+        printMatrix(mtx, m, n);
+        int[][] rotatedMtx = rotate(mtx, m, n);
+        printMatrix(rotatedMtx, rotatedMtx.length, rotatedMtx[0].length);
+    }
+
+    private static void printMatrix(int mtx[][], int m, int n) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(mtx[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
