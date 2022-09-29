9726ac2ee0727d68e477b7460b9fabe3176ecf10
nkchauhan003 <nkchauhan003@gmail.com>
2022-09-29 14:36:04 +0530

    added new problems

---
+++ src/main/java/com/cb/graph/G1_BreadthFirstSearch.java
@@ -0,0 +1,45 @@
+package com.cb.graph;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class G1_BreadthFirstSearch {
+
+    public static List<Integer> breadthFirstSearch(int v, List<List<Integer>> adj) {
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> ans = new ArrayList<>();
+
+        int[] visited = new int[v];
+
+        queue.add(0);
+        while (!queue.isEmpty()) {
+            int e = queue.poll();
+            if (visited[e] > 0)
+                continue;
+            ans.add(e);
+            visited[e] = 1;
+
+            if (!adj.get(e).isEmpty())
+                queue.addAll(adj.get(e));
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int v = 5;
+
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < v; i++)
+            adj.add(new ArrayList<>());
+
+        adj.get(0).add(1);
+        adj.get(0).add(2);
+        adj.get(0).add(3);
+        adj.get(2).add(4);
+
+        System.out.println(breadthFirstSearch(v, adj));
+    }
+}
