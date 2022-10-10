2b882acb4c04cc1d0f0bfe296a509fd25d81b28f
nkchauhan003 <nkchauhan003@gmail.com>
2022-10-10 13:06:43 +0530

    added new problems

---
+++ src/main/java/com/cb/stack/ST1_ImplementStackUsingArray.java
@@ -0,0 +1,59 @@
+package com.cb.stack;
+
+import java.util.NoSuchElementException;
+
+/*
+ * Stack using array
+ * */
+public class ST1_ImplementStackUsingArray {
+    private int[] arr;
+    private int top;
+
+    private int size;
+
+    public ST1_ImplementStackUsingArray(int size) {
+        this.arr = new int[size];
+        this.top = -1;
+        this.size = size;
+    }
+
+    public void push(int e) {
+        if (top >= size - 1)
+            throw new StackOverflowError();
+        arr[++top] = e;
+    }
+
+    public int pop() {
+        if (top == -1)
+            throw new NoSuchElementException();
+        return arr[top--];
+    }
+
+    public int top() {
+        if (top == -1)
+            throw new NoSuchElementException();
+        return arr[top];
+    }
+
+    public int size() {
+        return top + 1;
+    }
+}
+
+class Impl {
+    public static void main(String[] args) {
+        ST1_ImplementStackUsingArray stack = new ST1_ImplementStackUsingArray(3);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        System.out.println(stack.size());
+
+        System.out.println(stack.top());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
+        stack.push(4);
+        stack.push(5);
+    }
+}
