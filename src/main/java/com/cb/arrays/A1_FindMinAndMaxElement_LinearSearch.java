b781fe988f4c45c01c75933174e63bdb46b39bc5
nkchauhan003 <nkchauhan003@gmail.com>
2022-10-15 11:04:21 +0530

    added new problems

--- src/main/java/com/cb/arrays/A1_FindMinAndMaxElement_LinearSearch.java
+++ src/main/java/com/cb/arrays/A1_FindMinAndMaxElement_LinearSearch.java
@@ -15,12 +15,12 @@ public class A1_FindMinAndMaxElement_LinearSearch {
         if (input == null || input.length < 1)
             return new long[]{-1, -1};
 
-        // initialize with extreme values
-        long min = Long.MAX_VALUE;
-        long max = 0;
+        // initialize with 0th element
+        long min = input[0];
+        long max = input[0];
 
         // start iteration from 2nd element
-        for (int i = 0; i < input.length; i++) {
+        for (int i = 1; i < input.length; i++) {
             if (input[i] > max)
                 max = input[i];
             if (input[i] < min)
@@ -31,10 +31,10 @@ public class A1_FindMinAndMaxElement_LinearSearch {
 
     // Testing
     public static void main(String[] args) {
-        printArray(findMinMax(new long[]{}));
-        printArray(findMinMax(new long[]{1, 2, 3, 4, 5}));
-        printArray(findMinMax(new long[]{1}));
-        printArray(findMinMax(new long[]{52, 94, 3, 18, 5}));
+        printArray(findMinMax(new long[]{})); // -1,-1
+        printArray(findMinMax(new long[]{1, 2, 3, 4, 5})); // 1,5
+        printArray(findMinMax(new long[]{1})); // 1,1
+        printArray(findMinMax(new long[]{52, 94, 3, 18, 5})); // 3,94
     }
 
     // Just to print output array
