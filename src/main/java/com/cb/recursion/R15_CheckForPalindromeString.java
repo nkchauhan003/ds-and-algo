a6f5d8924995d47536f3d614c40d7983c2c03eec
nkchauhan003 <nkchauhan003@gmail.com>
2022-10-21 16:46:42 +0530

    added new problems

--- src/main/java/com/cb/recursion/R15_CheckForPalindromeString.java
+++ src/main/java/com/cb/recursion/R15_CheckForPalindromeString.java
@@ -22,18 +22,18 @@ public class R15_CheckForPalindromeString {
 
     public static void main(String[] args) {
         String s1 = "a";
-        System.out.println(isPalindrome(s1, 0, s1.length() - 1));
+        System.out.println(isPalindrome(s1, 0, s1.length() - 1)); // true
 
         String s2 = "ab";
-        System.out.println(isPalindrome(s2, 0, s2.length() - 1));
+        System.out.println(isPalindrome(s2, 0, s2.length() - 1)); // false
 
         String s3 = "xyz";
-        System.out.println(isPalindrome(s3, 0, s3.length() - 1));
+        System.out.println(isPalindrome(s3, 0, s3.length() - 1)); // false
 
         String s4 = "aba";
-        System.out.println(isPalindrome(s4, 0, s1.length() - 1));
+        System.out.println(isPalindrome(s4, 0, s1.length() - 1)); // true
 
         String s5 = "abcdcba";
-        System.out.println(isPalindrome(s5, 0, s5.length() - 1));
+        System.out.println(isPalindrome(s5, 0, s5.length() - 1)); // true
     }
 }
