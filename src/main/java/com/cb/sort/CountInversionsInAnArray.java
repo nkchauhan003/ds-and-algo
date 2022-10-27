105c3f9f69fa9aaba8ac5d2b2e67c82476dc3cc9
nkchauhan003 <nkchauhan003@gmail.com>
2022-10-27 11:44:42 +0530

    added new problems

--- src/main/java/com/cb/sort/CountInversionsInAnArray.java
+++ src/main/java/com/cb/sort/CountInversionsInAnArray.java
@@ -2,23 +2,25 @@ package com.cb.sort;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
+
 /*
-* Time: O(n log n)
-* Space: O(n)
-* */
-public class MergeSort {
-    public static void mergeSort(int[] arr, int low, int high) {
+ * Time: O(n log n)
+ * Space: O(n)
+ * */
+public class CountInversionsInAnArray {
+
+    public static void mergeSortAndCount(int[] arr, int low, int high) {
         if (low >= high) return;
 
         int mid = (low + high) / 2;
 
-        mergeSort(arr, low, mid);
-        mergeSort(arr, mid + 1, high);
+        mergeSortAndCount(arr, low, mid);
+        mergeSortAndCount(arr, mid + 1, high);
 
-        merge(arr, low, mid, high);
+        mergeAndCount(arr, low, mid, high);
     }
 
-    public static void merge(int[] arr, int low, int mid, int high) {
+    public static void mergeAndCount(int[] arr, int low, int mid, int high) {
 
         int n = mid - low + 1;
         int m = high - mid;
@@ -36,14 +38,14 @@ public class MergeSort {
 
         while (i < n && j < m) {
 
-            if (left[i] <= right[j]) {
-                arr[k] = left[i];
-                i++;
-            } else {
-                arr[k] = right[j];
-                j++;
-            }
-            k++;
+                if (left[i] <= right[j]) {
+                    arr[k] = left[i];
+                    i++;
+                } else {
+                    arr[k] = right[j];
+                    j++;
+                }
+                k++;
 
         }
         while (i < n) {
@@ -62,11 +64,12 @@ public class MergeSort {
     public static void main(String[] args) {
         int arr[] = {38, 27, 43, 3, 9, 82, 10};
         printArray(arr);
-        mergeSort(arr, 0, arr.length - 1);
+        mergeSortAndCount(arr, 0, arr.length - 1);
         printArray(arr);
     }
 
     private static void printArray(int[] arr) {
         System.out.println(Arrays.stream(arr).mapToObj(Integer::toString).collect(Collectors.joining(",")));
     }
+
 }
