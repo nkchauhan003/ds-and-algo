8afee2cd232d10285c3982cbaa7f476bf1ae8da6
nkchauhan003 <nkchauhan003@gmail.com>
2023-01-19 14:15:43 +0530

    added new problems

--- src/main/java/com/cb/binarytree/BT17_CheckIfBinaryTreeIsSumTree.java
+++ src/main/java/com/cb/binarytree/BT17_CheckIfBinaryTreeIsSumTree.java
@@ -24,19 +24,18 @@ public class BT17_CheckIfBinaryTreeIsSumTree {
     }
 
     public static void main(String[] args) {
-        isSumTree(tree());
+        System.out.println(isSumTree(tree())); // false
     }
 
     private static Node<Integer> tree() {
-        Node<Integer> root = new Node(1);
-        root.left = new Node(2);
-        root.right = new Node(3);
-        root.left.left = new Node(4);
+        Node<Integer> root = new Node(20);
+        root.left = new Node(8);
+        root.right = new Node(12);
+        root.left.left = new Node(3);
         root.left.right = new Node(5);
-        root.right.left = new Node(6);
+        root.right.left = new Node(5);
         root.right.right = new Node(7);
-        root.right.left.right = new Node(8);
-        root.right.right.left = new Node(9);
+        root.right.left.right = new Node(5);
 
         return root;
     }
