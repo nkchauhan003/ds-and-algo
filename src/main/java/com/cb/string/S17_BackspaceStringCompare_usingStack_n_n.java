d345e4861966ac99941e57ba239fd8c9cbeb20e4
nkchauhan003 <nkchauhan003@gmail.com>
2023-02-04 00:35:04 +0530

    added new problems

---
+++ src/main/java/com/cb/string/S17_BackspaceStringCompare_usingStack_n_n.java
@@ -0,0 +1,45 @@
+package com.cb.string;
+
+import java.util.Stack;
+
+/*
+ * Time complexity: O(n)
+ * Space complexity: O(n)
+ * */
+public class S17_BackspaceStringCompare_usingStack_n_n {
+
+    private static Stack<Character> finalCharStack(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c != '#') {
+                stack.push(c);
+            } else if (!stack.isEmpty()) {
+                stack.pop();
+            }
+        }
+        return stack;
+    }
+
+    private static boolean backspaceCompare(String s, String t) {
+
+        Stack<Character> st = finalCharStack(s);
+        Stack<Character> tt = finalCharStack(t);
+
+        if (st.size() != tt.size())
+            return false;
+
+        while (!st.isEmpty()) {
+            if (st.pop() != tt.pop())
+                return false;
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        String s = "abcd";
+        String t = "bbcd";
+        System.out.println(backspaceCompare(s, t));// true
+    }
+}
